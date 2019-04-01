c5b2b1717832c66e59cd05b670d6497388317e8c
gaojian <gaojian@idstaff.com>
2019-04-01 15:27:35 +0800

    iviewAdmin 框架提交

---
+++ src/view/qxgl/qxgl.vue
@@ -0,0 +1,158 @@
+<template>
+  <Table border :columns="columns7" :data="data6"></Table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        columns7: [
+          {
+            title: '姓名',
+            key: 'name',
+            render: (h, params) => {
+              return h('div', [
+                h('Icon', {
+                  props: {
+                    type: 'person'
+                  }
+                }),
+                h('strong', params.row.name)
+              ]);
+            }
+          },
+          {
+            title: '邮箱',
+            key: 'age'
+          },
+          {
+            title: '所属部门',
+            key: 'address'
+          },
+          {
+            title: '用户类型',
+            key: 'address'
+          },
+          {
+            title: '开通时间',
+            key: 'address'
+          },
+          {
+            title: '开通人',
+            key: 'address'
+          },
+          {
+            title: '最后变更时间',
+            key: 'address'
+          },
+          {
+            title: '权限变更人',
+            key: 'address'
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 300,
+            align: 'center',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.index)
+                    }
+                  }
+                }, '权限'),
+
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.index)
+                    }
+                  }
+                }, '修改'),
+
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params.index)
+                    }
+                  }
+                }, '删除'),
+
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.remove(params.index)
+                    }
+                  }
+                }, '角色')
+              ]);
+            }
+          }
+        ],
+        data6: [
+          {
+            name: 'John Brown',
+            age: 18,
+            address: 'New York No. 1 Lake Park'
+          },
+          {
+            name: 'Jim Green',
+            age: 24,
+            address: 'London No. 1 Lake Park'
+          },
+          {
+            name: 'Joe Black',
+            age: 30,
+            address: 'Sydney No. 1 Lake Park'
+          },
+          {
+            name: 'Jon Snow',
+            age: 26,
+            address: 'Ottawa No. 2 Lake Park'
+          }
+        ]
+      }
+    },
+    methods: {
+      show (index) {
+        window.location.href = '';
+
+        this.$Modal.info({
+          title: 'User Info',
+          content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
+        })
+      },
+      remove (index) {
+        this.data6.splice(index, 1);
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
